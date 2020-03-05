7b2fffcb68f2d98eae7d1e7d8b11c1d2f3d79188
Tyonek Thompson <tyonekthompson@Tyoneks-MBP.cable.rcn.com>
2020-03-04 19:18:36 -0500

    retaurant id

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const api = '2ce4ead64f6aba19ba40cdbd3a3963c6';
 const urlSearch = 'https://developers.zomato.com/api/v2.1/search';
 const urlSearchId = 'https://developers.zomato.com/api/v2.1/restaurant';
+const urlSearchLocation = 'https://developers.zomato.com/api/v2.1/locations';
 const option = {
 	headers : new Headers({
 		'user-key' : api
@@ -9,13 +10,27 @@ const option = {
 
 //search for restaurants by name
 const getDataByName = (restaurantSearch, citySearch) => {
-	const params = {};
+	getLocation(citySearch)
+	.then(city => { console.log(city)
+		const params = {};
 	let newUrl;
 	params.q = restaurantSearch;
-	params.entity_id = citySearch;
+	params.entity_id = city.location_suggestions[0].entity_id;
 	const searchParams = $.param(params);
 	newUrl = `${urlSearch}?${searchParams}`;
 	fetch(newUrl, option).then((res) => res.json()).then((resjson) => console.log(resjson.restaurants));
+	})
+	
+};
+
+//get location by name
+const getLocation = (citySearch) => {
+	const params = {};
+	let newUrl;
+	params.query = citySearch;
+	const searchParams = $.param(params);
+	newUrl = `${urlSearchLocation}?${searchParams}`;
+	return fetch(newUrl, option).then((res) => res.json());
 };
 
 //get info by restaurant id
