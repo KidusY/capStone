4ac96dffd9bcd8d14b24720e1c822e2160e9df30
Tyonek Thompson <tyonekthompson@Tyoneks-MBP.cable.rcn.com>
2020-03-05 11:39:21 -0500

    MVP

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+'use strict'
+
 const api = '2ce4ead64f6aba19ba40cdbd3a3963c6';
 const urlSearch = 'https://developers.zomato.com/api/v2.1/search';
 const urlSearchId = 'https://developers.zomato.com/api/v2.1/restaurant';
 const urlSearchLocation = 'https://developers.zomato.com/api/v2.1/locations';
 const urlDiscover = 'https://developers.zomato.com/api/v2.1/collections';
 const option = {
-	headers : new Headers({
-		'user-key' : api
+	headers: new Headers({
+		'user-key': api
 	})
 };
 //render results for search restaurant by name
@@ -44,14 +46,17 @@ const renderHtmlDiscover = (restaurants) => {
 	renderHtml(restaurants);
 };
 //search for restaurants by name
-const getDataByName = (restaurantSearch, citySearch) => {
-	const params = {};
-	let newUrl;
-	params.q = restaurantSearch;
-	params.entity_id = citySearch;
-	const searchParams = $.param(params);
-	newUrl = `${urlSearch}?${searchParams}`;
-	fetch(newUrl, option).then((res) => res.json()).then((resjson) => renderHtml(resjson.restaurants));
+const getDataByName = (restaurantSearch, cityName) => { console.log(restaurantSearch, cityName);
+	getLocation(cityName).then(cityData => {
+		const params = {};
+		let newUrl;
+		params.q = restaurantSearch;
+		params.entity_type = "city"
+		params.entity_id = cityData.location_suggestions[0].city_id;
+		const searchParams = $.param(params);
+		newUrl = `${urlSearch}?${searchParams}`;
+		fetch(newUrl, option).then((res) => res.json()).then((resjson) => renderHtml(resjson.restaurants));
+	})
 };
 
 
@@ -63,10 +68,10 @@ const getLocation = (citySearch) => {
 	params.query = citySearch;
 	const searchParams = $.param(params);
 	newUrl = `${urlSearchLocation}?${searchParams}`;
-	fetch(newUrl, option)
+	return fetch(newUrl, option)
 		.then((res) => res.json())
-		//passes city id to discover
-		.then((resjson) => discover(resjson.location_suggestions[0].city_id));
+	//passes city id to discover
+	// .then((resjson) => discover(resjson.location_suggestions[0].city_id));
 };
 
 //get info by restaurant id
@@ -80,22 +85,29 @@ const getDataById = (id) => {
 };
 
 //get different restaurants by city Id
-const discover = (cityId) => {
-	console.log(cityId);
-	const params = {};
-	let newUrl;
-	params.city_id = cityId;
-	const searchParams = $.param(params);
-	newUrl = `${urlDiscover}?${searchParams}`;
-	//collections obj
-	fetch(newUrl, option).then((res) => res.json()).then((resjson) => render(resjson.collections));
+const discover = (cityName) => {
+	getLocation(cityName).then(cityData => {
+		console.log(cityData);
+		const params = {};
+		let newUrl;
+		params.city_id = cityData.location_suggestions[0].city_id
+		params.entity_type = cityData.location_suggestions[0].entity_type
+		const searchParams = $.param(params);
+		newUrl = `${urlDiscover}?${searchParams}`;
+		//collections obj
+		fetch(newUrl, option).then((res) => res.json()).then((resjson) => render(resjson.collections));
+	})
 };
 
 const render = (collections) => {
 	$(".collections").show()
-	for(const collection of collections) { console.log(collection)
+	for (const collection of collections) {
+		console.log(collection)
 		$(".collections").append(`<div data-target=${collection.collection.collection_id}>
-		${collection.collection.title}
+		<h2>${collection.collection.title}</h2>
+		<p>${collection.collection.description}</p>
+		<img src="${collection.collection.image_url}">
+		<p>${collection.collection.url}</p>
 		</div>`)
 	}
 }
@@ -112,29 +124,34 @@ const collections = (collections) => {
 
 //events
 const events = () => {
-	$('.widget1').click(function (e){
+	$('.widget1').click(function (e) {
 		e.preventDefault();
 		$('main section').hide();
 		$('.searchRestaurant').show();
 	});
-	$('.widget2').click(function (e){
+	$('.widget2').click(function (e) {
 		e.preventDefault();
 		$('main section').hide();
 		$('.search').show();
 	});
-	$('.collections').on("click", "div", function (e){
-		e.preventDefault();collections($(this)[0].dataset.target)
-		
+	$('.collections').on("click", "div", function (e) {
+		e.preventDefault(); collections($(this)[0].dataset.target)
+
+	})
+	//returns to landing page on h1 click
+	$('.searchRestaurant', 'search').on("click", "h1", function (e) {
+		e.preventDefault();
+		$('.landingPage').show();
 	})
 };
 
-function main (){
+function main() {
 	events();
 	//gets info for search restaurant by name
 	$('.searchRestaurantForm').on('submit', (e) => {
 		e.preventDefault();
 		const restaurantSearch = $('#restaurantSearch').val().trim();
-		const citySearch = $('#citySearch').val().trim();
+		const citySearch = $('#citySearchRestaurant').val().trim();
 		getDataByName(restaurantSearch, citySearch);
 		$('.results').show();
 	});
@@ -143,7 +160,7 @@ function main (){
 	$('.discoverForm').on('submit', (e) => {
 		e.preventDefault();
 		const citySearch = $('#citySearch').val().trim();
-		getLocation(citySearch);
+		discover(citySearch);
 	});
 
 	//after selecting a restaurant, search info by Id
